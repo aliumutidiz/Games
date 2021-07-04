e1a064870a50450f53751cae022271deada17363
Psychorot <68105991+Psychorot@users.noreply.github.com>
2021-07-04 15:38:58 +0300

    optipack

--- Minecraft/Pack/OptiFine_1.16.5_HD_U_G8.lua
+++ Minecraft/Pack/OptiFine_1.16.5_HD_U_G8.lua
@@ -1,3 +1,3 @@
-dwn = "https://optifine.net/downloadx?f=OptiFine_1.16.5_HD_U_G8.jar&x=1ebf1a6b6f5f24174aa69cca4f2c9ba3"
+dwn = "https://optifine.net/adloadx?f=OptiFine_1.16.5_HD_U_G8.jar&x=afba"
 --OptiFine_1.16.5_HD_U_G8
 return dwn
--- Minecraft/Pack/opt.lua
+++ Minecraft/Pack/opt.lua
@@ -5,4 +5,4 @@ WebClient=luanet.import_type("System.Net.WebClient")
 istemci = WebClient()
  
 
-return istemci:DownloadString('https://optifine.net/adloadx?f=OptiFine_1.16.5_HD_U_G8.jar&x=1b30')
+return istemci:DownloadString('https://optifine.net/')
