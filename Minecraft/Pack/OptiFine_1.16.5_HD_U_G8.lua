95331df1f728b958c8ea4413849d6959195462e7
Byte <68105991+PsychoRot@users.noreply.github.com>
2022-05-20 14:00:41 +0300

    -

--- Minecraft/Pack/OptiFine_1.16.5_HD_U_G8.lua
+++
@@ -1,3 +0,0 @@
-dwn = "https://optifine.net/adloadx?f=OptiFine_1.16.5_HD_U_G8.jar&x=afba"
---OptiFine_1.16.5_HD_U_G8
-return dwn
--- Minecraft/Pack/opt.lua
+++
@@ -1,8 +0,0 @@
-luanet.load_assembly("System")
-WebClient=luanet.import_type("System.Net.WebClient")
- 
-
-istemci = WebClient()
- 
-
-return istemci:DownloadString('https://optifine.net/')
